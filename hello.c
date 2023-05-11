dfdf099facc5672eafae5204abe33cb1a6f56dd5
Seth Ketsi <christambassador12@gmail.com>
2023-05-11 13:31:04 +0000

    C Program Test

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+int ch;
+
+printf("Please type in one character:\n");
+ch = getc(stdin);
+printf("The Character you just entered is: %c\n", ch);
+return 0;
+
+}
+
+

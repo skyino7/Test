cd4777803c111556c96217c1929a47743abf51fe
Seth Ketsi <christambassador12@gmail.com>
2024-02-16 23:43:07 +0000

    OPENAI API Test

--- openaitest.mjs
+++ openaitest.mjs
@@ -1,15 +1,16 @@
-import OpenAI from "openai"
-require('dotenv').config()
+import OpenAI from "openai";
+require('dotenv').config();
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 async function main() {
 
     const completion = await openai.chat.completions.create({
         messages: [{"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}],
+            {"role": "user", "content": "write and html code to sign up a form"},
+            // {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            // {"role": "user", "content": "Where was it played?"}],
+    ],
         model: "gpt-3.5-turbo",
     });
 
